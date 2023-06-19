66995cc5a3072d29f084218f51e9e0619838fcb6
Takuzen Toh <ttoh@my-mac-mini.lan>
2023-06-19 18:10:37 +0900

    fix: single quote to double quotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
 import './globals.css'
-import { FC, ReactNode } from 'react'
 import { Metadata } from 'next'
  
 export const metadata: Metadata = {
   title: 'notan-office',
 }
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-const RootLayout: FC<RootLayoutProps> = ({ children }) => {
+// These styles apply to every route in the application
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <>
-      <style>@import url('https://fonts.googleapis.com/css2?family=Yatra+One&display=swap');</style>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
-      <div className="font-inter">{children}</div>
-    </>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   )
 }
-
-export default RootLayout
