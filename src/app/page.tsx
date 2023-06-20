b3bb17a427ce81ca9ef3ee907b600e6fc52161fd
Takuzen Toh <ttoh@my-mac-mini.lan>
2023-06-20 12:15:14 +0900

    feat: perfect the floating window UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,32 +49,37 @@ export default function Home() {
           <h1 className='font-yatra text-3xl sm:text-4xl text-gray-300 cursor-pointer' onClick={handleNotanSPClick}>
             notanSP
           </h1>
-
         </div>
       </div>
 
       {showFloatingWindow && (
-        <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 px-10 py-20 sm:px-10 sm:py-20 bg-white border border-gray-300 rounded-xl shadow-lg text-center leading-loose tracking-widest">
-          <button className="absolute top-2 right-2 w-6 h-6 flex items-center justify-center rounded-full bg-gray-300" onClick={handleCloseClick}>
-            <span className="text-white text-xs">X</span>
-          </button>
-          <p>個人用<br/>紙とデジタル上の<br/>ノートテイキング<br/>統合サービス</p>
-          <br/>
-          <Link href="https://notan.vercel.app">
-            <p className='underline underline-offset-4'>サービスへ進む</p>
-          </Link>
-        </div>
+        <>
+          <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50"></div>
+          <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 px-10 py-20 sm:px-10 sm:py-20 bg-white border border-gray-300 rounded-xl shadow-lg text-center leading-loose tracking-widest">
+            <button className="absolute top-2 right-2 w-6 h-6 flex items-center justify-center rounded-full bg-gray-300" onClick={handleCloseClick}>
+              <span className="text-white text-xs">X</span>
+            </button>
+            <p>個人用<br/>紙とデジタル上の<br/>筆記統合サービス</p>
+            <br/>
+            <Link href="https://notan.vercel.app">
+              <p className='underline underline-offset-4'>サービスへ進む</p>
+            </Link>
+          </div>
+        </>
       )}
 
       {showFloatingWindowSP && (
-        <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 px-10 py-20 sm:px-10 sm:py-20 bg-white border border-gray-300 rounded-xl shadow-lg text-center leading-loose tracking-widest">
-          <button className="absolute top-2 right-2 w-6 h-6 flex items-center justify-center rounded-full bg-gray-300" onClick={handleCloseClick}>
-            <span className="text-white text-xs">X</span>
-          </button>
-          <p>個人用<br/>空間コンピューティング向けの<br/>電子書籍リーダー</p>
-          <br/>
-          <p className='text-yellow-500'>Apple Vision Pro<br/>と同時リリース</p>
-        </div>
+        <>
+          <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50"></div>
+          <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 px-10 py-20 sm:px-10 sm:py-20 bg-white border border-gray-300 rounded-xl shadow-lg text-center leading-loose tracking-widest">
+            <button className="absolute top-2 right-2 w-6 h-6 flex items-center justify-center rounded-full bg-gray-300" onClick={handleCloseClick}>
+              <span className="text-white text-xs">X</span>
+            </button>
+            <p>個人用<br/>空間コンピューティング向けの<br/>電子書籍リーダー</p>
+            <br/>
+            <p className='text-yellow-500'>Apple Vision Pro<br/>と同時リリース</p>
+          </div>
+        </>
       )}
     </main>
   );
